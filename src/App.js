bf13acc359ccdf9221f26ce5be21067a6ae65db1
Jihoon Kim <kimjigabk@gmail.com>
2019-06-06 23:05:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn 개씨발ㄴㄴㄴㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
         </a>
       </header>
     </div>
